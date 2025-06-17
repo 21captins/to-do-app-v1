4d6ae1d964794417e284de3a0a07a92c2e617ee1
21captins <167474367+21captins@users.noreply.github.com>
2025-06-17 04:30:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,30 +16,16 @@ taskButton.addEventListener("click", function(){
 
 
     const dih = document.createElement("div")
-    dih.setAttribute("id", "dih-box")
+    
 
     const listItem = document.createElement("li")
     const task = taskInput.value
-    listItem.textContent = task
-    listItem.style.fontSize = "1rem"
-    listItem.style.fontFamily = "'Inter', Arial, sans-serif";
-
     const deleteItem = document.createElement("button")
     const clearItem = document.createElement("button")
 
-
-    deleteItem.addEventListener("click", function(){
-        dih.remove()
-        const deleteItem = new Audio("sound/delete.mp3")
-        deleteItem.play()
-    })
-    clearItem.addEventListener("click", function(){
-        listItem.style.color = "gray"
-        listItem.style.textDecoration = "line-through";
-        const complete = new Audio("sound/chime1-trimmed.mp3")
-        complete.play()
-    })
-
+    listItem.textContent = task
+    listItem.style.fontSize = "1rem"
+    listItem.style.fontFamily = "'Inter', Arial, sans-serif";
     deleteItem.textContent = "🗑️";
     clearItem.textContent = "✅";
 
@@ -47,17 +33,29 @@ taskButton.addEventListener("click", function(){
     clearItem.setAttribute("id","clear-button")
     deleteItem.classList.add("delete-button")
     clearItem.classList.add("clear-button")
+    dih.setAttribute("id", "dih-box")
 
     listItem.append(deleteItem)
     listItem.append(clearItem)
     dih.append(listItem)
     controlList.append(dih)
 
-    // Dynamically increase ul width
-    const baseWidth = 200; // starting width in px
-    const widthPerItem = 40; // px to add per item
-    const itemCount = controlList.children.length;
-    controlList.style.width = (baseWidth + widthPerItem * itemCount) + "px";
+    deleteItem.addEventListener("click", function(){
+        setTimeout(function(){dih.remove()},100)
+        const deleteItem = new Audio("sound/delete.mp3")
+        deleteItem.play()
+    })
+    clearItem.addEventListener("click", function(){
+        setTimeout(function(){listItem.style.color = "gray"
+        listItem.style.textDecoration = "line-through";
+        const complete = new Audio("sound/chime1-trimmed.mp3")
+        complete.play()}, 50)
+    })
 
     taskInput.value = "";
+
+    const wordCount = task.trim().split(/\s+/).length;
+    if(wordCount >= 20){
+        listItem.style.fontSize = "0.7rem"
+    }
 })
